ad310492d2b6172cddc675ecfd6351bda806d0a3
NebulaScout <ribak427@gmail.com>
2026-01-17 21:43:35 +0300

    Create user registration and login forms

---
+++ src/pages/AuthPages.jsx
@@ -0,0 +1,205 @@
+import { useState } from "react";
+import { FaEyeSlash, FaEye } from "react-icons/fa";
+import AuthPagesNavBar from "@/components/AuthPagesNavBar";
+import styles from "@/styles/authpages.module.css";
+import { Link } from "react-router-dom";
+
+export default function Login() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { key, value } = e.target;
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <div className={styles.pageContainer}>
+        <AuthPagesNavBar />
+
+        <div className={styles.container}>
+          <h2 className={styles.title}>Log in to your account</h2>
+
+          <form onSubmit={handleSubmit}>
+            <div className={styles.inputGroup}>
+              <label className={styles.label}>Email:</label>
+
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={styles.input}
+                required
+              />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <div className={styles.labelRow}>
+                <label>Password:</label>
+                <button
+                  type="button"
+                  className={styles.togglePassword}
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}{" "}
+                </button>
+              </div>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <a href="#" className={styles.forgotPassword}>
+              Forgot Password?
+            </a>
+            <button className={styles.btnSubmit} type="submit">
+              Log in
+            </button>
+          </form>
+          <div className={styles.divider}>
+            <hr />
+          </div>
+
+          <p className={styles.loginText}> Don't have have an account? </p>
+          <button className={styles.btnLogOutline}>Sign up</button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export function Register() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { key, value } = e.target;
+    setFormData((prev) => ({ ...prev, [key]: value }));
+  };
+
+  const handleRedirect = () => {
+    <Link to="/login">Login</Link>;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
+  return (
+    <>
+      <div className={styles.pageContainer}>
+        <AuthPagesNavBar />
+
+        <div className={styles.container}>
+          <h2 className={styles.title}>Sign up for an account</h2>
+
+          <form onSubmit={handleSubmit}>
+            <div className={styles.inputGroup}>
+              <label className={styles.label}>Email:</label>
+
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                className={styles.input}
+                required
+              />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label>First Name:</label>
+
+              <input
+                type="text"
+                name="firstName"
+                value={formData.firstName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label>Last Name:</label>
+
+              <input
+                type="text"
+                name="lastName"
+                value={formData.lastName}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <div className={styles.labelRow}>
+                <label>Password:</label>
+                <button
+                  type="button"
+                  className={styles.togglePassword}
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <FaEye /> : <FaEyeSlash />}{" "}
+                </button>
+              </div>
+              <input
+                type={showPassword ? "text" : "password"}
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className={styles.inputGroup}>
+              <label>Confirm Password</label>
+
+              <input
+                type={showPassword ? "text" : "password"}
+                name="confirmPassword"
+                value={formData.confirmPassword}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <a href="#" className={styles.forgotPassword}>
+              Forgot Password?
+            </a>
+            <button className={styles.btnSubmit} type="submit">
+              Sign Up
+            </button>
+          </form>
+          <div className={styles.divider}>
+            <hr />
+          </div>
+
+          <p className={styles.loginText}> Already have an account? </p>
+          <button className={styles.btnLogOutline} onClick={handleRedirect}>
+            Log in
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
